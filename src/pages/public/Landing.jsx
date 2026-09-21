b83789e216be6f6c96ad2fb9ba39f5a20f233530
alexanderst2005-lab <alexanderst2005@gmail.com>
2026-09-21 16:05:34 -0500

    fix: landing syntax error

--- src/pages/public/Landing.jsx
+++ src/pages/public/Landing.jsx
@@ -330,7 +330,8 @@ export default function App() {
                     </button>
                   </div>
                 </div>
-              ))}
+                );
+              })}
             </div>
           )}
         </div>
